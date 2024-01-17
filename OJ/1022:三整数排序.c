e1a8f5fa7e54360b82bbef9e8d56031324a828c5
HaoZheng <mail@mail.com>
2024-01-17 13:35:25 +0800

    empty

--- OJ/1022:三整数排序.c
+++ OJ/1022:三整数排序.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int x, y, z;
-    scanf("%d %d %d", &x, &y, &z);
+  int x, y, z;
+  scanf("%d %d %d", &x, &y, &z);
 
-    if (x >= y && x >= z) {
-        if (y >= z) {
-            printf("%d %d %d", x, y, z);
-        } else {
-            printf("%d %d %d", x, z, y);
-        }
-    } else if (y >= x && y >= z) {
-        if (x >= z) {
-            printf("%d %d %d", y, x, z);
-        } else {
-            printf("%d %d %d", y, z, x);
-        }
+  if (x >= y && x >= z) {
+    if (y >= z) {
+      printf("%d %d %d", x, y, z);
     } else {
-        if (x >= y) {
-            printf("%d %d %d", z, x, y);
-        } else {
-            printf("%d %d %d", z, y, x);
-        }
+      printf("%d %d %d", x, z, y);
     }
+  } else if (y >= x && y >= z) {
+    if (x >= z) {
+      printf("%d %d %d", y, x, z);
+    } else {
+      printf("%d %d %d", y, z, x);
+    }
+  } else {
+    if (x >= y) {
+      printf("%d %d %d", z, x, y);
+    } else {
+      printf("%d %d %d", z, y, x);
+    }
+  }
 
-    return 0;
+  return 0;
 }
--- OJ/1027:判断水仙花数.c
+++ OJ/1027:判断水仙花数.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int num;
-    
-    // 从标准输入读取一个三位数
-    printf("请输入一个三位数：");
-    scanf("%d", &num);
+  int num;
 
-    // 提取百位、十位和个位数字
-    int hundreds = num / 100;
-    int tens = (num % 100) / 10;
-    int units = num % 10;
+  // 从标准输入读取一个三位数
+  printf("请输入一个三位数：");
+  scanf("%d", &num);
 
-    // 判断是否为水仙花数并输出相应信息
-    if (num == (hundreds * hundreds * hundreds + tens * tens * tens + units * units * units)) {
-        printf("yes\n");
-    } else {
-        printf("no\n");
-    }
+  // 提取百位、十位和个位数字
+  int hundreds = num / 100;
+  int tens = (num % 100) / 10;
+  int units = num % 10;
 
-    return 0;
+  // 判断是否为水仙花数并输出相应信息
+  if (num == (hundreds * hundreds * hundreds + tens * tens * tens +
+              units * units * units)) {
+    printf("yes\n");
+  } else {
+    printf("no\n");
+  }
+
+  return 0;
 }
